a2cc182dba8d03b58d62f9dfb03a36b692baa40c
randilwelikala <randilgimantha646@gmail.com>
2025-04-14 22:20:04 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const names =[
+  {id:0 , name:"John"},
+  {id:1 , name:"Jane"},
+  {id:2 , name:"Alice"},
+  {id:3 , name:"Bob"},
+  {id:4 , name:"Eve"},
+
+];
 
 function App() {
+
+  const[count,setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         
+          
+        </p>
         
       </header>
     </div>
